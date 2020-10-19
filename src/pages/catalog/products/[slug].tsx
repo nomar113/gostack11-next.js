84009941f617b6ebbb591bfd82dcc31f8336b6d5
Ramon Mesquita <ramon.mesquita@zoom.com.br>
2020-10-19 18:04:04 -0300

    Dinamic routes

---
+++ src/pages/catalog/products/[slug].tsx
@@ -0,0 +1,12 @@
+import { route } from "next/dist/next-server/server/router";
+import { useRouter } from "next/router";
+
+export default function Product() {
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+    </div>
+  )
+}
